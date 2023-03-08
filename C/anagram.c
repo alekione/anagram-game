cd23c8ed365ee2c0ebb24e1d836dc0bd7da5a3c8
alekione <lexa9448@gmail.com>
2023-03-08 21:45:35 +0300

    remove bug on game level 2 and 3 in guess_word

--- C/anagram.c
+++ C/anagram.c
@@ -6,7 +6,7 @@
 char *create_word(char *word, int level)
 {
 	char *ret = strdup(word);
-	unsigned int *rand_num[] = {NULL, NULL, NULL, NULL};
+	unsigned int rand_num[4];
 	unsigned int num, i, j;
 
 	srand(time(0));
@@ -18,36 +18,32 @@ char *create_word(char *word, int level)
 	if (level == 2)
 	{
 		for (i = 0; i < 2; i++)
-			rand_num[i] = (unsigned int *)(rand() % strlen(word));
-		rand_num[i] = NULL;
+			rand_num[i] = (unsigned int)(rand() % strlen(word));
 		for (i = 0; i < strlen(word); i++)
 		{
 			for (j = 0; j < 2; j++)
-				if (*rand_num[j] == i)
+				if (rand_num[j] == i)
 					ret[i] = '_';
 		}
 	}
 	if (level == 3)
 	{
 		for (i = 0; i < 3; i++)
-			rand_num[i] = (unsigned int *)(rand() % strlen(word));
-		rand_num[i] = NULL;
+			rand_num[i] = (unsigned int)(rand() % strlen(word));
 		for (i = 0; i < strlen(word); i++)
 		{
 			for (j = 0; j < 3; j++)
-				if (*rand_num[j] == i)
+				if (rand_num[j] == i)
 					ret[i] = '_';
 		}
 	}
-	for (i = 0; rand_num[i] != NULL; i++)
-		free(rand_num[i]);
 	return(ret);
 }
 
 int main()
 {
 	char *word = "sleepy";
-	int level = 1;
+	int level = 3;
 	printf("%s\n",create_word(word, level));
 	return 0;
 }
